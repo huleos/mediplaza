c643c1dea3e3f00d656b13d5681e224eb40c159b
Julio Cesar Montenegro <creativo@686studio.com>
2014-12-13 14:26:54 -0800

    off canvas contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,20 @@ $(document).ready(function(){
       $('.navbar-collapse').removeClass("open");
   });
 
+  // home navigation
   $('.home-nav li').hover(function(){
 		$(".cover-box", this).stop().animate({top:'100%'},{queue:false,duration:300});
 	}, function() {
 		$(".cover-box", this).stop().animate({top:'0px'},{queue:false,duration:300});
 	});
+
+  // Navigation
+  $(".contact-button > a").click(function(){
+    $('body').toggleClass("show-contact");
+  });
+  
+  $("#close-contact").click(function(){
+      $('body').removeClass("show-contact");
+  });
 
 });
